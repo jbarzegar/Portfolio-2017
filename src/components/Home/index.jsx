580b84719c0b4597c8569b556b0dbe1f33adbae7
James Barzegar <james.barzegar@gmail.com>
2017-05-07 23:38:36 -0400

    Added more stuff

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,13 +2,14 @@ import { h } from 'preact';
 import styles from './Home.scss';
 
 import About from '../About/';
+import Skills from '../Skills';
 
 const Home = () => (
     <div className="Home">
         <section className={`flex align-center ${styles.Hero} ${styles.Landing}`}>
             <header>
                 <h1 className={styles.heading}>
-                    <span>Hi!</span>
+                    <span>Hi! &#128075;</span>
                     <span>I'm James.</span>
                     <span>I'm a Full Stack</span>
                     <span>Web Developer.</span>
@@ -16,7 +17,10 @@ const Home = () => (
             </header>
         </section>
         <div className={styles.siteContainer}>
-            <About />
+            <section className={styles.innerWrap}>
+                <About />
+                <Skills />
+            </section>
         </div>
     </div>
 );
---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,101 @@
+import { h } from 'preact';
+import styles from './Skills.scss';
+
+const handleClick = (event) => {
+    if (event.type === 'touchstart') {
+        // Don't trigger mouseenter even if they hold
+        event.stopImmediatePropagation();
+        // If $item is a link (<a>), don't go to said link on mobile, show menu instead
+        event.preventDefault();
+    }
+};
+
+const Skills = () => (
+    <div className={styles.Styles}>
+        <h2>The Toolkit.</h2>
+        <section className={`flex flex-wrap ${styles.cardContainer}`}>
+            <section className={`flex-col align-all-center ${styles.card} ${styles.javascriptCard}`} onClick={handleClick}>
+                <i className="devicon-javascript-plain" />
+                <div className={styles.lowerContainer}>
+                    <h3>JavaScript</h3>
+                </div>
+                <ul className={`flex-col align-all-center ${styles.libList}`}>
+                    <li className="es6">
+                        ES6
+                    </li>
+                    <li className="react">
+                        React
+                    </li>
+                    <li className="angular">
+                        AngularJS
+                    </li>
+                    <li className="nodejs">
+                        NodeJS
+                    </li>
+                </ul>
+            </section>
+            <section className={`flex-col align-all-center ${styles.card} ${styles.pythonCard}`} onClick={handleClick}>
+                <i className="devicon-python-plain" />
+                <div className={styles.lowerContainer}>
+                    <h3>Python</h3>
+                </div>
+                <ul className={`flex-col align-all-center ${styles.libList}`}>
+                    <li className="2.7">
+                        2.7
+                    </li>
+                    <li className="3.5+">
+                        3.x
+                    </li>
+                    <li className="django">
+                       Django
+                    </li>
+                    <li className="flask">
+                        Flask
+                    </li>
+                </ul>
+            </section>
+            <section className={`flex-col align-all-center ${styles.card} ${styles.devOpsCard}`} onClick={handleClick}>
+                <i className="fa fa-terminal" />
+                <div className={styles.lowerContainer}>
+                    <h3>Devops</h3>
+                </div>
+                <ul className={`flex-col align-all-center ${styles.libList}`}>
+                    <li className="docker">
+                        Docker
+                    </li>
+                    <li className="automation">
+                        Automation
+                    </li>
+                    <li className="mongo">
+                        Mongo
+                    </li>
+                    <li className="postgres">
+                        Postgres
+                    </li>
+                </ul>
+            </section>
+            <section className={`flex-col align-all-center ${styles.card} ${styles.frontEnd}`} onClick={handleClick}>
+                <i className="fa fa-code" />
+                <div className={styles.lowerContainer}>
+                    <h3>Front-End</h3>
+                </div>
+                <ul className={`flex-col align-all-center ${styles.libList}`}>
+                    <li className="html">
+                        HTML5
+                    </li>
+                    <li className="css">
+                        Css/Scss
+                    </li>
+                    <li className="gulp">
+                        Gulp
+                    </li>
+                    <li className="webpack">
+                        Webpack
+                    </li>
+                </ul>
+            </section>
+        </section>
+    </div>
+);
+
+export default Skills;
